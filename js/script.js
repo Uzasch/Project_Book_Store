d7414e7fdc75f087f29789ea0e46b8c9cddc1cd1
Uzasch <uzair2021.cheulkar@gmail.com>
2023-08-02 16:24:08 +0530

    icons add after section added

--- js/script.js
+++ js/script.js
@@ -25,3 +25,33 @@ window.onload = () =>{
     }
 }
 
+$(document).ready(function() {
+    $('.slider').slick({
+        slidesToShow: 2, // Display 4 images at the same time
+        slidesToScroll: 1,
+        centerMode:true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+});
