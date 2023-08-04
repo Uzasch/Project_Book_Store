3cba873a16a2fc9143c71ed4905f6c0fab4e8f05
Uzasch <uzair2021.cheulkar@gmail.com>
2023-08-04 10:58:31 +0530

    newsletter made

--- js/script.js
+++ js/script.js
@@ -55,3 +55,35 @@ $(document).ready(function() {
         ]
     });
 });
+
+
+$(document).ready(function() {
+    $('.slider2').slick({
+        slidesToShow: 4, // Display 4 images at the same time
+        slidesToScroll: 1,
+        centerMode:true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+});
