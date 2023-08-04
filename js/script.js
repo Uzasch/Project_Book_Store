0fbd9d0c4c930ef4ac417480f9cca1eab52cb2cb
Uzasch <uzair2021.cheulkar@gmail.com>
2023-08-04 13:06:27 +0530

    arrival section done

--- js/script.js
+++ js/script.js
@@ -27,9 +27,9 @@ window.onload = () =>{
 
 $(document).ready(function() {
     $('.slider').slick({
-        slidesToShow: 2, // Display 4 images at the same time
-        slidesToScroll: 1,
-        centerMode:true,
+        slidesToShow: 2, 
+        slidesToScroll: 2,
+        centerMode:false,
         autoplay: true,
         autoplaySpeed: 3000,
         infinite: true,
@@ -63,10 +63,18 @@ $(document).ready(function() {
         slidesToScroll: 1,
         centerMode:true,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 2000,
         infinite: true,
         responsive: [
-            {
+            {   
+                
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {   
+                
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 2,
@@ -75,7 +83,7 @@ $(document).ready(function() {
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 1,
                 }
             },
             {
@@ -87,3 +95,85 @@ $(document).ready(function() {
         ]
     });
 });
+
+
+$(document).ready(function() {
+    $('.slider3').slick({
+        slidesToShow: 4, // Display 4 images at the same time
+        slidesToScroll: 3,
+        centerMode:true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        infinite: true,
+        responsive: [
+            {   
+                
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {   
+                
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+});
+
+
+$(document).ready(function() {
+    $('.slider4').slick({
+        slidesToShow: 4, // Display 4 images at the same time
+        slidesToScroll: 3,
+        centerMode:true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        infinite: true,
+        responsive: [
+            {   
+                
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {   
+                
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+});
+
+
